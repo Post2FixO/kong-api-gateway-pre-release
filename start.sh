3a518ed5f5dc5ea3c3b032c6a921445c2eaa2172
Post2Fix <127422443+Post2FixO@users.noreply.github.com>
2023-06-08 23:06:34 +0200

    Added instructions, comments and made the following improvements:
    - Update docker-compose build command to explicitly specify service name (kong) to be built for clarity
    
    - Use docker-compose up -d instead of docker-compose start to ensure containers are started in detached mode

--- start.sh
+++ start.sh
@@ -1,10 +1,33 @@
-docker compose build kong
-docker compose up -d kong-db
-docker compose run --rm kong kong migrations bootstrap
-docker compose run --rm kong kong migrations up
-docker compose up -d kong
+#!/bin/bash
+
+# Build the Kong image
+docker-compose build kong
+
+# Start the Kong database container
+docker-compose up -d kong-db
+
+# Bootstrap Kong migrations
+docker-compose run --rm kong kong migrations bootstrap
+
+# Run Kong migrations
+docker-compose run --rm kong kong migrations up
+
+# Start the Kong service
+docker-compose up -d kong
+
+# Check the available Kong plugins
 curl -s http://localhost:8001 | jq .plugins.available_on_server.oidc
-docker compose up -d konga
+
+# Start the Konga UI
+docker-compose up -d konga
+
+# Wait for the services to fully start up (adjust sleep duration as needed)
 sleep 90s
-docker compose up -d keycloak-db
-docker compose up -d keycloak
+
+# Start the Keycloak database container
+docker-compose up -d keycloak-db
+
+# Start the Keycloak service
+docker-compose up -d keycloak
+
+# After modifications, ensure start.sh has executable permissions (chmod +x start.sh) and test the script to verify that it starts services as expected
