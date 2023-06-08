0f9d47fd7717afcf7e885c7c7bc52e21aef2eb54
Post2Fix <127422443+Post2FixO@users.noreply.github.com>
2023-06-08 19:49:03 +0200

    Create x86-macos-start.sh

---
+++ x86-macos-start.sh
@@ -0,0 +1,10 @@
+docker-compose build kong
+docker-compose up -d kong-db
+docker-compose run --rm kong kong migrations bootstrap
+docker-compose run --rm kong kong migrations up
+docker-compose up -d kong
+curl -s http://localhost:8001 | jq .plugins.available_on_server.oidc
+docker-compose up -d konga
+sleep 90s
+docker-compose up -d keycloak-db
+docker-compose up -d keycloak
